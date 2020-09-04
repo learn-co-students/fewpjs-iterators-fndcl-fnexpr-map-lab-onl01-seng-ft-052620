73ab58ea8c34c99ac561080a7eb2d9a1434017c9
KaelinAlexander <kaelin.alexander@gmail.com>
2020-09-04 11:22:22 -0600

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,21 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  // console.log('TESTING', arg)
+  // tutorials.map(function(tutorial) {
+  //   tutorial.toLowerCase().split(' ').map(function(word) {
+  //     return (word.charAt(0).toUpperCase() + word.slice(1));
+  //   })
+// })
+tutorials.map((tutorial) => {
+  let testToken = tutorial.split(' ')
+  let capTestToken = testToken.map((word) => {
+    word.charAt(0).toUpperCase() + word.slice(1)
+  })
+  let response = capTestToken.join(' ')
+  console.log(response)
+  // tutorial.toLowerCase().split(' ').map(function(word) {
+  //   console.log((word.charAt(0).toUpperCase()));
+  // })
+  // return tutorials
+})}
